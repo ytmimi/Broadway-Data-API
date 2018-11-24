787ce24e2bb610b1c54892cc3441e442ee6b9414
Yacin Tmimi <yacintmimi@gmail.com>
2018-11-24 12:48:42 -0500

    added graphene scheam settings

--- broadway_database/broadway_database/settings.py
+++ broadway_database/broadway_database/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
     'api',
+
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'broadway_database.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
